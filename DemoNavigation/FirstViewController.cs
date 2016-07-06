5ec9538f39a88ab4c0923d066570a12b51512a89
Dheeraj Kumar Gunti <guntidheerajkumar@gmail.com>
2016-07-06 16:11:38 +0530

    Working with UINavigationController in Xamarin iOS
    Working with UINavigationController in Xamarin iOS

---
+++ DemoNavigation/FirstViewController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+
+namespace DemoNavigation
+{
+	public class FirstViewController : UIViewController
+	{
+		public FirstViewController ()
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			this.View.BackgroundColor = UIColor.White;
+			this.NavigationController.NavigationBarHidden = false;
+			this.Title = "First View Controller";
+
+			var label = new UILabel (new RectangleF (10, 40, 300, 30));
+			label.Text = "First View Controller";
+			label.TextAlignment = UITextAlignment.Center;
+			this.View.Add (label);
+
+			var button = new UIButton (UIButtonType.Custom);
+			button.SetTitle ("Move To Next", UIControlState.Normal);
+			button.SetTitleColor (UIColor.Blue, UIControlState.Normal);
+			button.TouchUpInside += (object sender, EventArgs e) => {
+				this.NavigationController.PushViewController (new SecondViewController (), true);
+			};
+			button.Frame = new RectangleF (10, 70, 200, 40);
+
+			this.View.Add (button);
+		}
+	}
+}
+
---
+++ DemoNavigation/SecondViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+
+namespace DemoNavigation
+{
+	public class SecondViewController : UIViewController
+	{
+		public SecondViewController ()
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			this.View.BackgroundColor = UIColor.White;
+
+			var label = new UILabel (new RectangleF (10, 40, 300, 30));
+			label.Text = "Second View Controller";
+			label.TextAlignment = UITextAlignment.Center;
+			this.View.Add (label);
+
+			var button = new UIButton (UIButtonType.Custom);
+			button.SetTitle ("Move To Previous", UIControlState.Normal);
+			button.SetTitleColor (UIColor.Blue, UIControlState.Normal);
+			button.Frame = new RectangleF (10, 70, 200, 40);
+
+			this.View.Add (button);
+		}
+	}
+}
+
